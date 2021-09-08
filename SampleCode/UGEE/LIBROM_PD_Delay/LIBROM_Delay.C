fc961656a6503c573a26c54ad99acb84c0dd1447
MB20 LCWang0 <LCWANG0@nuvoton.com>
2021-09-08 11:31:59 +0800

    MUG51 BSP

---
+++ SampleCode/UGEE/LIBROM_PD_Delay/LIBROM_Delay.C
@@ -0,0 +1,78 @@
+/*---------------------------------------------------------------------------------------------------------*/
+/*                                                                                                         */
+/* Copyright(c) 2021 Nuvoton Technology Corp. All rights reserved.                                         */
+/*                                                                                                         */
+/*---------------------------------------------------------------------------------------------------------*/
+
+//***********************************************************************************************************
+//  Website: http://www.nuvoton.com
+//  E-Mail : MicroC-8bit@nuvoton.com
+//  Date   : Apr/29/2021
+//***********************************************************************************************************
+
+//***********************************************************************************************************
+//  File Function: MUG51 GPIO toggle demo code
+//***********************************************************************************************************
+#include "MUG51.h"
+#include "librom.h"
+
+unsigned char ct;
+void WakeUp_Timer_ISR (void)   interrupt 17
+{
+_push_(SFRS);
+
+    if(ct==10)
+    {
+        SET_EXIT_LIBROM_FLAG;
+    }
+    ct++;
+    clr_WKCON_WKTF;                     //clear interrupt flag
+    set_WKCON_WKTR;
+
+_pop_(SFRS);
+}
+
+  void WKTInit(void)
+{
+    SFRS = 0;
+    WKCON = 0x00;                     //timer base 10k, Pre-scale = 1/16
+    SFRS = 2;
+    RWKH = 0xFC;
+    SFRS = 0;
+    RWKL = 0;                         //  if prescale is 0x00, never set RWK = 0xff
+    ENABLE_WKT_INTERRUPT;             // enable WKT interrupt
+    ENABLE_GLOBAL_INTERRUPT;
+    set_WKCON_WKTR;
+}
+
+void main(void)
+{
+
+  DISABLE_BOD;
+  ALL_GPIO_QUASI_MODE;
+  WKTInit();
+  EA =1;
+  LIBROM_PD_RETURN();
+  while(1)
+  {
+    P22^=1;
+    LIBROM_DELAY_10US();
+    P22^=1;
+    LIBROM_DELAY_50US();
+    P22^=1;
+    LIBROM_DELAY_100US();
+    P22^=1;
+    LIBROM_DELAY_1MS();
+    P22^=1;
+    LIBROM_DELAY_10MS();
+
+    if(ct==20)
+    { 
+    ct=0; 
+    CLR_EXIT_LIBROM_FLAG;
+    LIBROM_PD_RETURN();
+    }
+  }
+  while(1);
+}
+
---
+++ SampleCode/UGEE/Timer2_ACMP/Capture_reg.c
@@ -0,0 +1,77 @@
+/*---------------------------------------------------------------------------------------------------------*/
+/*                                                                                                         */
+/* Copyright(c) 2021 Nuvoton Technology Corp. All rights reserved.                                         */
+/*                                                                                                         */
+/*---------------------------------------------------------------------------------------------------------*/
+
+//***********************************************************************************************************
+//  Website: http://www.nuvoton.com
+//  E-Mail : MicroC-8bit@nuvoton.com
+//***********************************************************************************************************
+
+//***********************************************************************************************************
+//  File Function: MUG51 TIMER 2 Capture function demo
+//***********************************************************************************************************
+
+#include "MUG51.h"
+/**
+ * @brief       Timer2  interrupt subroutine
+ */
+unsigned char capvl, capvh;
+bit capflag;
+bit ecflag=0;
+bit capct=0;
+
+void Capture_ISR(void) interrupt 12      // Vector @  0x63
+{
+_push_(SFRS);
+
+//  if (!capct)
+//    goto CTFIRST;
+
+    SFRS=1;
+    capvl = C1L;
+    capvh= C1H;
+
+//    capct=0;
+//    ecflag =1;
+//    goto  CTINTEND;
+
+//CTFIRST:
+//  capct=1;
+//CTINTEND:
+  clr_CAPCON0_CAPF0;
+  clr_CAPCON0_CAPF1;
+_pop_(SFRS);
+}
+
+void ACMP_ISR (void) interrupt 23
+{
+_push_(SFRS);
+    ACMPSR&=CLR_BIT0;
+_pop_(SFRS);
+}
+
+void main (void) 
+{
+    MFP_P23_ACMP0_P1;
+    MFP_P24_ACMP0_N0;
+
+/*  Initial ACMP0, P23 as positive, P24 as Negitive, and Bandgap as negetive input, Output direct  */
+    ACMP_Open(ACMP0, ACMP_POSSEL_PIN_P1, ACMP_NEGSEL_PIN_N0, ACMP_CRV_DISABLE, ACMP_OUTPUT_DIRECT, ACMP_HYSTERESIS_ENABLE);
+    ACMP_Interrupt(ACMP0, ACMP_WAKEUP_ENABLE, ACMP_INTERRUPT_ENABLE);
+    TIMER2_Capture_Interrupt(ENABLE);
+
+    TIMER2_Clcok_Source(TM2_P40);
+    TIMER2_Capture(IC1,ACMP0_O,CAPTURERISING,0);
+    set_CAPCON2_CMOD;
+    Global_Interrupt(ENABLE);
+
+    while(1)
+    {
+      set_PCON_PD;
+    }
+}
+
+
+
